14aa9b442f3ab3e64aa8335491ad203b0d427b5f
fathallah <60329067+fathallahma@users.noreply.github.com>
2023-05-10 22:16:48 +0200

    Update home.component.spec.ts

--- ecommerce_Frontend/src/app/Content/home/home.component.spec.ts
+++ ecommerce_Frontend/src/app/Content/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HomeComponent } from './home.component';
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,16 +9,25 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the HomeComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display buttons for "Hommes", "Enfants" and "Femmes"', () => {
+    const buttons = fixture.nativeElement.querySelectorAll('button');
+    expect(buttons.length).toBe(3);
+    expect(buttons[0].textContent).toContain('Hommes');
+    expect(buttons[1].textContent).toContain('Enfants');
+    expect(buttons[2].textContent).toContain('Femmes');
+  });
+
 });
