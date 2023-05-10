4df3ab104879f9d91763cf64c3bc69b8e211aad8
fathallah <60329067+fathallahma@users.noreply.github.com>
2023-05-10 22:47:06 +0200

    Update header.component.spec.ts

--- ecommerce_Frontend/src/app/Structure/header/header.component.spec.ts
+++ ecommerce_Frontend/src/app/Structure/header/header.component.spec.ts
@@ -8,16 +8,40 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the HeaderComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the site logo', () => {
+    const logo = fixture.nativeElement.querySelector('img');
+    expect(logo.src).toContain('assets/img/logo.jpg');
+  });
+
+  it('should display navigation links for "Hommes", "Femmes" and "Enfants"', () => {
+    const navLinks = fixture.nativeElement.querySelectorAll('a[routerLink]');
+    expect(navLinks.length).toBe(3);
+    expect(navLinks[0].textContent).toContain('Hommes');
+    expect(navLinks[1].textContent).toContain('Femmes');
+    expect(navLinks[2].textContent).toContain('Enfants');
+  });
+
+  it('should display a search input field', () => {
+    const searchInput = fixture.nativeElement.querySelector('input[type="search"]');
+    expect(searchInput).toBeTruthy();
+  });
+
+  it('should display icons for "favorite" and "shopping_basket"', () => {
+    const icons = fixture.nativeElement.querySelectorAll('mat-icon[routerLink]');
+    expect(icons.length).toBe(2);
+    expect(icons[0].textContent).toContain('favorite');
+    expect(icons[1].textContent).toContain('shopping_basket');
   });
 });
